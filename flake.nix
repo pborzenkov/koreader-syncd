b4296009d1719cbe56e1b865b1aef034e7a9ac23
Pavel Borzenkov <pavel@borzenkov.net>
2023-12-05 14:43:11 +0100

    Switch back to flake.nix
    No need for devenv really

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "koreader-syncd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      toolchain = fenix.packages.${system}.fromToolchainFile {
+        file = ./rust-toolchain.toml;
+        sha256 = "sha256-U2yfueFohJHjif7anmJB5vZbpP7G6bICH4ZsjtufRoU=";
+      };
+    in {
+      packages.default =
+        (pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        })
+        .buildRustPackage {
+          name = "koreader-syncd";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [
+          self.packages.${system}.default
+        ];
+
+        buildInputs = [
+          pkgs.cargo-nextest
+          pkgs.rust-analyzer
+        ];
+      };
+    });
+}
